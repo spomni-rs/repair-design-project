8b5460dd063452b83a669fde09756a446aa6b2e1
spomni <mikhail.snitko@gmail.com>
2019-10-05 08:56:43 +0300

    tool: Create gulp task to build html files.

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -2,6 +2,4 @@ const gulp = require('gulp')
 const {series, parallel} = gulp;
 const requireDir = require('require-dir');
 
-const tasks = requireDir('./tasks')
-
-module.exports = {}
+requireDir('./tasks')
